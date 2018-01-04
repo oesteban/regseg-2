4c41a18a8ebf1fd13f794adfc86e1deda5bbb5cf
Oscar Esteban <code@oscaresteban.es>
2018-01-03 16:55:43 -0800

    Update surf.py

--- regseg/workflows/surf.py
+++ regseg/workflows/surf.py
@@ -119,7 +119,4 @@ def _read_model(model_name):
               'rb' if version_info[0] < 3 else 'r') as sfh:
         model = json.load(sfh)
 
-    name = ['%s.surf' % for m in model.keys()]
-    labels = list(model.values())
-
-    return name, labels
+    return list(model.keys()), list(model.values())
